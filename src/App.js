bdbcf8755c46b47542faff886538e1968d22289e
samkwan2001 <51446044+samkwan2001@users.noreply.github.com>
2025-05-21 03:14:30 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -139,7 +139,8 @@ function App() {
           document.getElementById("welcome")
           && document.getElementById("welcome").style.display != "none"
         ){
-          try { if (eventSource !== void 0) {
+          try { if(cookie.load("_id"))fetch(`${API_BASE_URL}/close_${cookie.load("_id")}`);
+                if (eventSource !== void 0) {
                   eventSource.close();
                   eventSource=null;
                   cookie.remove("_id");
